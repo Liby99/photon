5d91f2d734b5c713df3b184e6a918025525c6835
Liby Lee <liby99@icloud.com>
2019-07-13 10:07:48 -0400

    Render texcoord

--- native/src/lib.rs
+++ native/src/lib.rs
@@ -14,22 +14,23 @@ fn another_hello(mut cx: FunctionContext) -> JsResult<JsString> {
 fn render(mut cx: FunctionContext) -> JsResult<JsBuffer> {
   let data = cx.argument::<JsBuffer>(0)?;
   let w = cx.number(255);
-  let b = cx.number(0);
   for x in 0..50 {
     for y in 0..50 {
       let index = (y * 50 + x) * 4;
-      data.set(&mut cx, index, w)?;
-      data.set(&mut cx, index + 1, b)?;
-      data.set(&mut cx, index + 2, b)?;
+      let r = cx.number(x * 5);
+      let g = cx.number(y * 5);
+      data.set(&mut cx, index, r)?;
+      data.set(&mut cx, index + 1, g)?;
+      data.set(&mut cx, index + 2, w)?;
       data.set(&mut cx, index + 3, w)?;
     }
   }
   Ok(data)
 }
 
-register_module!(mut m, {
-  m.export_function("hello", hello)?;
-  m.export_function("another_hello", another_hello)?;
-  m.export_function("render", render)?;
+register_module!(mut cx, {
+  cx.export_function("hello", hello)?;
+  cx.export_function("another_hello", another_hello)?;
+  cx.export_function("render", render)?;
   Ok(())
 });
