6f1b0f348e60c77901f24bc6baee81ed857b986e
Liby Lee <liby99@icloud.com>
2019-07-13 14:52:16 -0400

    Able to fill in the whole canvas

--- core/photon_live/lib/index.js
+++ core/photon_live/lib/index.js
@@ -1,3 +1,17 @@
-var addon = require('../native');
+// var addon = require('../native');
 
-console.log(addon.hello());
+// console.log(addon.hello());
+
+module.exports = {
+  fill(buffer, x, y, width, height, [r, g, b]) {
+    for (let i = 0; i < width; i++) {
+      for (let j = 0; j < height; j++) {
+        const index = ((y + j) * width + x + i) * 4;
+        buffer[index] = r;
+        buffer[index + 1] = g;
+        buffer[index + 2] = b;
+        buffer[index + 3] = 255;
+      }
+    }
+  }
+}
