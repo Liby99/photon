823b53d44d38c6cf565911061678c0498e60c0c5
Liby Lee <liby99@icloud.com>
2019-07-13 08:51:44 -0400

    Able to render red rectangle from rust

--- native/src/lib.rs
+++ native/src/lib.rs
@@ -11,7 +11,25 @@ fn another_hello(mut cx: FunctionContext) -> JsResult<JsString> {
   Ok(cx.string("another hello from rust"))
 }
 
-register_module!(mut cx, {
-  cx.export_function("hello", hello)?;
-  cx.export_function("another_hello", another_hello)
+fn render(mut cx: FunctionContext) -> JsResult<JsBuffer> {
+  let data = cx.argument::<JsBuffer>(0)?;
+  let w = cx.number(255);
+  let b = cx.number(0);
+  for x in 0..50 {
+    for y in 0..50 {
+      let index = (y * 50 + x) * 4;
+      data.set(&mut cx, index, w)?;
+      data.set(&mut cx, index + 1, b)?;
+      data.set(&mut cx, index + 2, b)?;
+      data.set(&mut cx, index + 3, w)?;
+    }
+  }
+  Ok(data)
+}
+
+register_module!(mut m, {
+  m.export_function("hello", hello)?;
+  m.export_function("another_hello", another_hello)?;
+  m.export_function("render", render)?;
+  Ok(())
 });
